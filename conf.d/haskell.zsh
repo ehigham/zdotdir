7a42afc3da52b19f511350b9fef2bed87c975560
Edmund Higham <edhigham@gmail.com>
2024-01-30 19:58:46 -0500

    combine all xdg flags into one directory

--- conf.d/haskell.zsh
+++
@@ -1,3 +0,0 @@
-# set to anything to use XDG style directories
-export GHCUP_USE_XDG_DIRS=1
-export STACK_XDG=1
---
+++ conf.d/xdg-dirs.zsh
@@ -0,0 +1,6 @@
+# docker
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
+
+# haskell
+export GHCUP_USE_XDG_DIRS=1
+export STACK_XDG=1
