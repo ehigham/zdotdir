6ba871fd9c670e260f7420683993698102e1ea8f
Edmund Higham <edhigham@gmail.com>
2024-01-30 20:22:48 -0500

    more cleanup of $HOME

--- conf.d/xdg-dirs.zsh
+++ conf.d/xdg-dirs.zsh
@@ -1,6 +1,19 @@
+# cargo
+export CARGO_HOME="${XDG_DATA_HOME}/cargo"
+
 # docker
 export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
+
+# go
+export GOPATH="${XDG_DATA_HOME}/go"
+export GOMODCACHE="${XDG_CACHE_HOME}/go/mod"
 
 # haskell
 export GHCUP_USE_XDG_DIRS=1
 export STACK_XDG=1
+
+# pyenv
+export PYENV_ROOT="${XDG_DATA_HOME}/pyenv"
+
+# z
+export _Z_DATA="$XDG_DATA_HOME/z"
