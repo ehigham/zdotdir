479198fe7b51866ff0e62f832f27904a617c7d15
Edmund Higham <edhigham@gmail.com>
2024-04-12 12:07:02 -0400

    npm xdg dirs

--- conf.d/xdg-dirs.zsh
+++ conf.d/xdg-dirs.zsh
@@ -15,5 +15,12 @@ export STACK_XDG=1
 # pyenv
 export PYENV_ROOT="${XDG_DATA_HOME}/pyenv"
 
+# npm
+# see npm config ls -l | grep /
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
+export NPM_CONFIG_CACHE=$XDG_CACHE_HOME/npm
+export NPM_CONFIG_TMP=$XDG_RUNTIME_DIR/npm
+export PATH=$PATH:$XDG_DATA_HOME/npm/bin
+
 # z
 export _Z_DATA="$XDG_DATA_HOME/z"
